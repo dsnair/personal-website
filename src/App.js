343e2ca66758926058925b38b5efe23e24a379d6
Divya Nair <divnairs@gmail.com>
2018-06-02 19:11:27 -0700

    Styling contact form
    Co-authored-by: Clinton Reece <clintonreece@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Nav from "./Nav";
 import Main from "./Main";
-import "./App.css";
 
 class App extends Component {
   render() {
--- src/Contact.js
+++ src/Contact.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import "./Contact.css";
-import githubLogo from "./images/github-logo.ico"
-import linkedinLogo from "./images/linkedin-logo.png"
-import gmailLogo from "./images/gmail-logo.ico"
+import githubLogo from "./images/github-logo.ico";
+import linkedinLogo from "./images/linkedin-logo.png";
+import gmailLogo from "./images/gmail-logo.ico";
 
 class Contact extends Component {
   render() {
@@ -12,37 +12,26 @@ class Contact extends Component {
           <p>
             I'd love to grab coffee and talk about project prospects with you!
           </p>
-          <form action="#" method="post">
-            <fieldset>
-              <input
-                type="text"
-                id="name"
-                name="user_name"
-                placeholder="Name"
-                autofocus="True"
-              />
-              <input
-                type="email"
-                id="email"
-                name="user_mail"
-                placeholder="Email"
-              />
-              <textarea
-                type="text"
-                id="msg"
-                name="user_msg"
-                placeholder="Message"
-              />
+          <form class="contact-items-form" action="#" method="post">
+            <div class="form-name">
+              <label for="name">Name</label>
+              <input type="text" id="name" autofocus="True" />
+            </div>
+            <div class="form-email">
+              <label for="email">Email</label>
+              <input type="email" id="email" />
+            </div>
+            <div class="form-msg">
+              <label for="msg">Message</label>
+              <textarea type="text" id="msg" />
+            </div>
+            <div class="form-button">
               <button type="submit">Send</button>
-            </fieldset>
+            </div>
           </form>
-
           <figure>
             <a href="https://github.com/dsnair" title="GitHub">
-              <img
-                src={githubLogo}
-                alt="https://github.com/dsnair"
-              />
+              <img src={githubLogo} alt="https://github.com/dsnair" />
             </a>
             <a href="https://www.linkedin.com/in/dsnair/" title="LinkedIn">
               <img
@@ -51,11 +40,7 @@ class Contact extends Component {
               />
             </a>
             <a href="mailto:nowhere@gmail.com">
-              <img
-                src={gmailLogo}
-                alt="nowhere@gmail.com"
-                title="Email"
-              />
+              <img src={gmailLogo} alt="nowhere@gmail.com" title="Email" />
             </a>
           </figure>
         </div>
