f7546e28b2059e68c5c23b35345bc2da9eb02f85
Divya Nair <divnairs@gmail.com>
2018-06-15 17:18:29 -0700

    added nav bar

---
+++ scripts/navbar.js
@@ -0,0 +1,10 @@
+function openMenu() {
+  document.getElementById("navbar").style.display = "inline-flex";
+  document.getElementById("navbar").style.justifyContent = "space-around";
+  document.getElementById("navbar").style.alignItems = "center";
+  document.getElementById("navbar").style.width = "100%";
+}
+
+function closeMenu() {
+  document.getElementById("navbar").style.display = "none";
+}
